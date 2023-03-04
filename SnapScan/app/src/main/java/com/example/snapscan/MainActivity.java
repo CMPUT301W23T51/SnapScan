b437c818c8f1b24d9aa5c8ab1f4aef6476c90836
prabhjo1 <prabhjo1@ualberta.ca>
2023-03-04 12:53:58 -0700

    added a new profile branch

--- SnapScan/app/src/main/java/com/example/snapscan/MainActivity.java
+++ SnapScan/app/src/main/java/com/example/snapscan/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        System.out.println("hey");
 
         //getSupportActionBar().setTitle("SnapScan");
 
