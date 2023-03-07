aca78e9536a3e6672fd7a4d1bc3196d2683ea16b
vk-varun <97547208+vk-varun@users.noreply.github.com>
2023-03-06 23:03:24 -0700

    Update ProfileActivity.java

--- SnapScan/app/src/main/java/com/example/snapscan/ProfileActivity.java
+++ SnapScan/app/src/main/java/com/example/snapscan/ProfileActivity.java
@@ -1,14 +1,57 @@
 package com.example.snapscan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView nameTextView, qrcodesTextView, pointsTextView;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        nameTextView = findViewById(R.id.player_name);
+        qrcodesTextView = findViewById(R.id.qrcode_count);
+        pointsTextView = findViewById(R.id.points_count);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String userId = user.getUid();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(userId);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue(String.class);
+                int qrcodes = dataSnapshot.child("qrcodes").getValue(Integer.class);
+                int points = dataSnapshot.child("points").getValue(Integer.class);
+
+                nameTextView.setText(name);
+                qrcodesTextView.setText(String.valueOf(qrcodes));
+                pointsTextView.setText(String.valueOf(points));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
