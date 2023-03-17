d25b5813e9b5aa86be3dd36ff5366b3bb722dc58
Suvan <suvanjr1@gmail.com>
2023-03-17 16:37:59 -0600

    Updated the branch to contain only 1 app

--- Original/app/src/main/java/com/example/original/ui/qr/CaptureAct.java
+++
@@ -1,7 +0,0 @@
-package com.example.original.ui.qr;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureAct extends CaptureActivity {
-// nothing to be filled in as superclass contains all the necessary functionalities
-}
