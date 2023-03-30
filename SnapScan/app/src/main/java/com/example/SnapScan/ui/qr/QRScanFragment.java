b51d313b5c4065cb4ad4b6a041eb186aa2724ec6
Suvan <suvanjr1@gmail.com>
2023-03-30 04:58:42 -0600

    Opening My QR Recycler View

--- SnapScan/app/src/main/java/com/example/SnapScan/ui/qr/QRScanFragment.java
+++ SnapScan/app/src/main/java/com/example/SnapScan/ui/qr/QRScanFragment.java
@@ -15,7 +15,6 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.SnapScan.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
